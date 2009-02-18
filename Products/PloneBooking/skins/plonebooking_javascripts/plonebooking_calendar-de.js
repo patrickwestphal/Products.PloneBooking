460e843dfe43eeb9aeafbaac744c2c999ab67273
fsprenger <fsprenger@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-02-18 16:38:20 +0000

    german translations of js-file

---
+++ Products/PloneBooking/skins/plonebooking_javascripts/plonebooking_calendar-de.js
@@ -0,0 +1,14 @@
+Calendar._SDN = new Array
+("So",
+ "Mo",
+ "Di",
+ "Mi",
+ "Do",
+ "Fr",
+ "Sa",
+ "So");
+ Calendar._TT["ABOUT"] =
+"Wahl des Datums:\n" +
+"- Benutzen Sie die Buttons \xab, \xbb  um das Jahr zu selektieren\n" +
+"- Benutzen Sie die Buttons " + String.fromCharCode(0x2039) + ", " + String.fromCharCode(0x203a) + " um den Monat zu selektieren\n" +
+"- F\u00fcr eine Schnellauswahl halten Sie die Maustaste \u00fcber diesen Buttons fest.";
